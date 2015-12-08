d55a7b17fdca381cef506ab1590574ba4b924c72
James Youn <jamesyoun710@gmail.com>
2015-12-08 09:26:23 -0800

    Create dot layout

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var main = function(){
   $('.dropdown-toggle').click(function(){
     $('.dropdown-menu').toggle();
   });
+
+  $('.dot').click(function(){
+    var currentDot = $('.dot');
+    var nextDot = currentDot.next();
+
+    if(currentDot.last()){
+      nextDot = $('.dot').first();
+    }
+    currentDot.removeClass('.activeDot');
+    nextDot.addClass('.activeDot');
+  });
 };
 
 $(document).ready(main);
